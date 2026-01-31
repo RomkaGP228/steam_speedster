133dd45b3c175ed5b9273fe9a4f0caff58c0884d
geopad <r.kulakov@edu.centraluniversity.ru>
2026-01-31 19:53:54 +0300

    Made main.py logic and steam_path.py edited

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+import time
+from steam_path import get_windows_steam_path
+from data_parser import parse_app_id, parse_rate, parse_status
+from game_name import get_game_name
+def read_new_lines(file_path, last_position):
+    with open(file_path, "r", encoding="utf-8") as f:
+        f.seek(last_position)
+        lines = f.readlines()
+        new_position = f.tell()
+
+    return lines, new_position
+
+def main():
+    steam_path = get_windows_steam_path()
+    content_log = steam_path / "logs" / "content_log.txt"
+    for i in range(5):
+        lines, position = read_new_lines(content_log, 0)
+        rate = parse_rate(lines)
+        appid = parse_app_id(lines)
+        status = parse_status(lines)
+        if appid:
+            game_name = get_game_name(steam_path, appid)
+        else:
+            game_name = "Unknown game"
+        print(f"Minute: {i}/5 | game: {game_name} | status: {status} | rate: {rate}")
+        time.sleep(60)
+
+
+
+
+
+
+
 if __name__ == '__main__':
     pass
 
--- steam_path.py
+++ steam_path.py
@@ -1,54 +1,19 @@
-# import winreg
-#
-#
-# def get_windows_steam_path():
-#     try:
-#         main_path = r"Software\Valve\Steam"
-#         hkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, main_path)
-#         path, code = winreg.QueryValueEx(hkey, "SteamPath")
-#         hkey.Close()
-#         return path
-#     except FileNotFoundError:
-#         print("Steam not found.")
-#         return None
-#     except Exception as e:
-#         print(f"An error while reading: {e}")
-#         return None
-#
-#
-#
-#
-# steam_location = get_windows_steam_path()
-# if steam_location:
-#     print(f"Steam установлен в: {steam_location}")
+import winreg
 
-import os
-from pathlib import Path
+def get_windows_steam_path():
+    try:
+        main_path = r"Software\Valve\Steam"
+        hkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, main_path)
+        path, code = winreg.QueryValueEx(hkey, "SteamPath")
+        hkey.Close()
+        return path
+    except FileNotFoundError:
+        print("Steam not found.")
+        return None
+    except Exception as e:
+        print(f"An error while reading: {e}")
+        return None
 
-
-def get_linux_steam_path():
-    # Получаем путь к домашней директории пользователя
-    home = Path.home()
-
-    # Список возможных путей установки
-    possible_paths = [
-        home / ".local/share/Steam",
-        home / ".steam/steam",
-        home / ".var/app/com.valvesoftware.Steam/.local/share/Steam",  # Flatpak
-        home / "snap/steam/common/.local/share/Steam"  # Snap
-    ]
-
-    for path in possible_paths:
-        # Проверяем, существует ли директория и есть ли в ней папка steamapps
-        if path.exists() and (path / "steamapps").exists():
-            return str(path)
-
-    return None
-
-
-# Проверка
-path = get_linux_steam_path()
-if path:
-    print(f"Steam найден по пути: {path}")
-else:
-    print("Steam не найден.")
+steam_location = get_windows_steam_path()
+if steam_location:
+    print(f"Steam установлен в: {steam_location}")
